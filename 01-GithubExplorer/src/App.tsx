55d17513ef6e3d595036b8e3b231d856e965c939
Maironsantana <maironnscmt@gmail.com>
2022-06-08 22:42:43 -0300

    Usando TypeScript

--- 01-GithubExplorer/src/App.tsx
+++ 01-GithubExplorer/src/App.tsx
@@ -1,12 +1,8 @@
-import { Counter } from './components/Counter.jsx';
 import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App(){
     return (
-        <>
         <RepositoryList/>
-        <Counter />
-        </>
     );
 }
