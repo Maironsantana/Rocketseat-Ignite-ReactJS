cc244266d6c14cc7d06697a347535954c6702076
Maironsantana <maironnscmt@gmail.com>
2022-07-15 12:59:55 -0300

    capitulo 4 e 5

---
+++ 05-perfreact/components/ProductItem.tsx
@@ -0,0 +1,43 @@
+import { memo, useState } from 'react';
+import { AddProductToWishListProps } from './AddProductToWishList'
+import dynamic from 'next/dynamic';
+
+import lodash from 'lodash'
+
+const AddProductToWishList = dynamic<AddProductToWishListProps>(() => {
+    return import('./AddProductToWishList').then(nod => nod.AddProductToWishList)
+}, {
+    loading: () => <span>Carregando...</span>
+})
+
+interface ProductItemProps{
+    product:{
+        id: number;
+        price: number;
+        priceFormatted: string;
+        title: string;
+    }
+    onAddToWishList: (id: number) => void;
+}
+
+function ProductItemComponent({ product, onAddToWishList }: ProductItemProps){
+    const [isAddingToWishList, setIsAddingToWishList] = useState(false);
+
+    return(
+        <div>
+            {product.title} - <strong>{product.priceFormatted}</strong>
+            <button onClick={() => setIsAddingToWishList(true)}>Adicionar aos favoritos</button>
+            
+            { isAddingToWishList && (
+                <AddProductToWishList 
+                    onAddToWishList={() => onAddToWishList(product.id)}
+                    onRequestClose={() => setIsAddingToWishList(false)}
+                />
+            )}
+        </div>
+    );
+}
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+    return lodash.isEqual(prevProps.product, nextProps.product)
+})
